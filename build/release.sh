97547236fefbf349228ce8f8e0779d89f144cb62
Holger Seelig <holger.seelig@gmail.com>
2023-11-27 16:46:13 +0100

    Published version 1.0.6

--- build/release.sh
+++ build/release.sh
@@ -6,4 +6,7 @@ git add -A;
 git commit -am 'Published version '`npm pkg get version | sed 's/"//g'`;
 git push origin;
 
+git tag `npm pkg get version | sed 's/"//g'`;
+git push origin --tags;
+
 npm publish
